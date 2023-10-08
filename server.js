7154564c881930a44decda9014d7a146e9f0d71c
Fabricio Alves <123993244+fabricioalvess@users.noreply.github.com>
2023-10-08 17:48:45 -0300

    iniciou a criacao de rotas nativa; instensao nodemon; script dev...

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 import http from "http";
 const PORT = 3000;
+
+const rotas = {
+    "/":"curso node!",
+    "/livros": "livros",
+    "/autores":"Autores"
+}
 const server  = http.createServer((req,res)=>{
     res.writeHead(200,{"content-Type":"text/plain"});
-    res.end("Curso de Node.JS");
+    res.end(rotas[req.url]);
 });
 
 server.listen(PORT,()=>{
