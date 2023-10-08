2bcf39d8617f2e55863960226296f71f9d280199
Fabricio Alves <123993244+fabricioalvess@users.noreply.github.com>
2023-10-08 17:38:28 -0300

    servidor rodando

--- server.js
+++ server.js
@@ -1,1 +1,11 @@
 import http from "http";
+const PORT = 3000;
+const server  = http.createServer((req,res)=>{
+    res.writeHead(200,{"content-Type":"text/plain"});
+    res.end("Curso de Node.JS");
+});
+
+server.listen(PORT,()=>{
+    console.log("Servidor escultando!")
+});
+
