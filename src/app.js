80776b37eab68f86b2ac8b8050d50c2f460c75c8
Fabricio Alves <123993244+fabricioalvess@users.noreply.github.com>
2023-10-09 16:33:27 -0300

    CRUD verbos http

--- src/app.js
+++ src/app.js
@@ -6,22 +6,41 @@ app.use(express.json())
 const livros = [
    { 
         id: 1,
-        titlo: "Senhor dos Aneis"
+        titulo: "Senhor dos Aneis"
     },
     {
         id:2,
         titulo:"O Hobbit"
     }
 ];
+function buscaLivro(id){
+    return livros.findIndex(livro => {
+        return livro.id === Number(id)
+    })
+}
 app.get("/",(req,res)=>{
     res.status(200).send("Curso de node.Js");
 });
 app.get("/livros", (req,res)=>{
     res.status(200).json(livros)
 });
+app.get("/livros/:id",(req,res)=>{
+    const index = buscaLivro(req.params.id)
+    res.status(200).json(livros[index])
+})
 app.post("/livros",(req,res)=>{
     livros.push(req.body)
     res.status(201).send("cadastrado")
+})
+app.put("/livros/:id",(req,res)=>{
+    const index =  buscaLivro(req.params.id)
+    livros[index].titulo = req.body.titulo;
+    res.status(200).json(livros)
+})
+app.delete("/livros/:id",(req,res)=>{
+    const index = buscaLivro(req.params.id);
+    livros.splice(index,1)
+    res.status(200).send("excluido")
 })
 
 export default app
